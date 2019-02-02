aa4f9d90e9bbe00d09cd869275e49dd09a5ca691
Ankit Goyal <goyal.ankit.049@gmail.com>
2019-02-02 17:02:30 +0530

    portfolio, templates and url

--- portal/urls.py
+++ portal/urls.py
@@ -8,5 +8,8 @@ from portal import views
 app_name="portal"
 
 urlpatterns = [
+    url("^portfolio$", TemplateView.as_view(template_name='portfolio.html'), name='portfolio'),
+    url("^tour$", TemplateView.as_view(template_name='tour.html'), name='tour'),
+    url("^contact$", TemplateView.as_view(template_name='contact.html'), name='contact'),
     url("", TemplateView.as_view(template_name='index.html'), name='index')
 ]
